d489c7a0084aa6ab000a60b848f0eac47e49f169
kpk2714 <krishnapadakar05@gmail.com>
2025-02-27 22:45:51 +0530

    Remember-me-parameter-modified

--- src/app/login/login-request.ts
+++ src/app/login/login-request.ts
@@ -2,4 +2,8 @@ export class LoginRequest {
     username! : string ;
     password! : string ;
     rememberMe!: boolean;
+
+    constructor(rememberMe : any) {
+        this.rememberMe = rememberMe;
+    }
 }
